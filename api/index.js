cdf91bada8456507e4001e94fa63a491201c2e00
cmkim <cmkim1003@revu.net>
2024-09-11 14:33:45 +0900

    app.listen 방식 제거

--- api/index.js
+++ api/index.js
@@ -1,10 +1,4 @@
 // api/index.js
 const app = require('../app'); // 기존 app.js 파일 로드
 
-// 포트 및 서버 설정
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
 module.exports = app;
