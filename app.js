4bd88c3e72c5c584a421c88f36e0f21705ad5a29
cmkim <cmkim1003@revu.net>
2024-09-12 10:00:08 +0900

    view 엔진 제거

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cors = require('cors');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/auth/users');
-var loginRouter = require('./routes/auth/login');
-var testRouter = require('./routes/test');
-var reservationRouter = require('./routes/calender/reservation');
-
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/auth/users');
+const loginRouter = require('./routes/auth/login');
+const testRouter = require('./routes/test');
+const reservationRouter = require('./routes/calender/reservation');
 
 // Swagger 설정 추가
 const { swaggerUi, swaggerSpec } = require("./swagger");
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-//cors 설정
-app.use(cors());
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
+// CORS 설정
+const corsOptions = {
+  origin: '*', // 해당 URL 주소만 요청을 허락함
+  credentials: true // 사용자 인증이 필요한 리소스를 요청할 수 있도록 허용함
+}
+app.use(cors(corsOptions)) // cors 설정 미들웨어
+
+// Swagger API 문서화 미들웨어 설정
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -37,15 +38,18 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// 에러 핸들러
+app.use((err, req, res, next) => {
+  // 로컬 변수 설정, 개발 환경에서만 에러 메시지를 제공
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // 에러 페이지 렌더링
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message,
+    error: res.locals.error
+  });
 });
 
 
